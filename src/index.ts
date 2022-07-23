026d85cdf0d36b6a09f4c342281e816abaa7e8cd
Oscar Davila <oscar7822002@gmail.com>
2022-07-23 13:48:28 -0500

    Constructor de una clase

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@
     ===== Código de TypeScript =====
 */
 class Hero {
-  alterEgo: string;
-  edad: number;
-  nombreReal: number;
-
-  imprimirNombre() {
-    return this.alterEgo + ' ' + this.nombreReal;
-  }
+  // alterEgo: string;
+  // edad: number;
+  // nombreReal: number;
+  constructor( 
+    public alterEgo: string,
+    public edad: number,
+    public nombreReal: string
+  ) {}
 }
 
-const iroman = new Hero();
+const iroman = new Hero('Ironman', 45, 'Tony');
 
 console.log(iroman)
